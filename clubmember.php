cc945fb85315763e89fb183e666dde60f165575a
Md. Al-Amin opu <alamin.opu10@gmail.com>
2014-07-16 00:23:18 +0600

    Added activation function

--- clubmember.php
+++ clubmember.php
@@ -11,3 +11,27 @@ License: GPL2
 
 if(!defined("ABSPATH")) exit();
 
+/**
+* Activation functio
+*
+* Creating table when installing the plugin
+*/
+
+function clubmember_activation(){
+	global $wpdb;
+	$table_name = $wpdb->prefix."clubmember_users";
+
+	$sql = "CREATE TABLE IF NOT EXISTS $table_name(
+		`id` bigint(25) unsigned NOT NULL AUTO_INCREMENT,
+		`user_id` bigint(25) DEFAULT NULL,
+		`status` tinyint(1) DEFAULT '1',
+		PRIMARY KEY(`id`),
+		KEY `user_id` (`user_id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+	// db query 
+	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+	dbDelta($sql);
+}
+
+register_activation_hook( __FILE__ , "clubmember_activation" );
