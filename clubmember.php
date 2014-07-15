550c63f8f4aabb6205e5849f8c3fd6de5312881d
Md. Al-Amin opu <alamin.opu10@gmail.com>
2014-07-15 23:15:49 +0600

    Initial plugin files and folders created

---
+++ clubmember.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Clubmember 
+Plugin URI: http://alaminopu.me/plugins/
+Description: To manage different type of Clubs
+Author: Md. Al-Amin opu
+Author URI: http://alaminopu.me
+Version: 0.1
+License: GPL2
+*/
+
+if(!defined("ABSPATH")) exit();
+
