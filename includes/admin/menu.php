03543092a00ae793202b5270fa5a2be6fb97f815
Md. Al-Amin opu <alamin.opu10@gmail.com>
2014-07-18 20:32:29 +0600

    Added sub menu

--- includes/admin/menu.php
+++ includes/admin/menu.php
@@ -6,21 +6,31 @@
 function clubmember_admin_menu(){
 	/* top level menu */
 	add_menu_page(
-				"Club Member page", 
-		        "Club Member", 
+				"Club Members", 
+		        "Club Members", 
 		        "manage_options", 
-		        "club-member-admin-menu", 
-		        "clubmember_admin_page",
+		        "clubmember-view-all", 
+		        "clubmember_view_all_page",
 		        "",
 		        '28'
 		    );
-
-
+	add_submenu_page("clubmember-view-all","View All Member", "View All", "manage_options", "clubmember-view-all");
+	add_submenu_page("clubmember-view-all","Add New Member", "Add Member", "manage_options", "clubmember-add-member", "clubmember_add_member_page" );
 }
 
 add_action("admin_menu", "clubmember_admin_menu");
 
-
-function clubmember_admin_page(){
-	echo "Muu";
+/**
+* view all members
+*/
+function clubmember_view_all_page(){
+	echo "View all member";
 }
+
+/* Add new member page */
+function clubmember_add_member_page(){
+	echo "Add a new member";
+}
+
+
+
