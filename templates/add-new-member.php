5ccf8678a08cd594c222bef06b30d5e95c5574aa
Md. Al-Amin opu <alamin.opu10@gmail.com>
2014-07-23 04:22:28 +0600

    add-new-member form update

--- templates/add-new-member.php
+++ templates/add-new-member.php
@@ -1,38 +1,52 @@
-<?php
-add_action( 'admin_init', 'clubmember_register_mysettings' );
-function clubmember_register_mysettings() {
-
-	register_setting( 'clubmember_add', 'clubmember_name' );
-	register_setting( 'clubmemer_add', 'clubmember_department' );
-	register_setting( 'clubmember_add', 'clubmember_semester' );
-}
-
-?>
-
 <div class="wrap">
 <h2>Add New Member</h2>
 
-<form method="post" action="options.php">
-    <?php settings_fields( 'clubmember_add' ); ?>
-    <?php do_settings_sections( 'clubmember_add' ); ?>
+<?php
+$full_name = "";
+$department = "";
+$semester = "";
+$rollno = "";
+$email = "";
+$phone = "";
+
+?>
+
+<form name="clubmember-add-form" method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+    <input type="hidden" name="clubmember_hidden" value="Y">
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">Full Name</th>
-        <td><input type="text" name="clubmember_name" value="<?php echo get_option('clubmember_name'); ?>" /></td>
+            <th scope="row">Full Name</th>
+            <td><input type="text" name="clubmember_name" value="<?php echo $full_name; ?>" /></td>
         </tr>
          
         <tr valign="top">
-        <th scope="row">Department</th>
-        <td><input type="text" name="clubmember_department" value="<?php echo get_option('clubmember_department'); ?>" /></td>
+            <th scope="row">Department</th>
+            <td><input type="text" name="clubmember_department" value="<?php echo $department; ?>" /></td>
         </tr>
         
         <tr valign="top">
-        <th scope="row">Semester</th>
-        <td><input type="text" name="clubmember_semester" value="<?php echo get_option('clubmember_semester'); ?>" /></td>
+            <th scope="row">Semester</th>
+            <td><input type="text" name="clubmember_semester" value="<?php echo $semester; ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+            <th scope="row">Class Roll</th>
+            <td><input type="text" name="clubmember_rollno" value="<?php echo $rollno; ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+            <th scope="row">Email</th>
+            <td><input type="text" name="clubmember_email" value="<?php echo $email; ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+            <th scope="row">Phone</th>
+            <td><input type="text" name="clubmember_phone" value="<?php echo $phone; ?>" /></td>
         </tr>
     </table>
     
-    <?php submit_button(); ?>
+    <?php submit_button( "Add Member", "primary" ); ?>
+
 
 </form>
 </div>
